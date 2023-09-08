f848c99b1061c90c4099d9f6535aea9ed97723bb
Crowdbotics <engineering@crowdbotics.com>
2023-09-08 23:23:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cool-grass-dev-115052",
+  url: "https://cool-grass-dev-115052.botics.co",
+  api: "https://cool-grass-dev-115052.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
